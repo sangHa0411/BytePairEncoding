85047651b0864327d56a1f9e0bc9721c486354cb
sangha110495 <48673702+sangHa0411@users.noreply.github.com>
2021-09-15 01:38:39 +0900

    Update for glove embedding

--- dataset.py
+++ dataset.py
@@ -4,42 +4,58 @@ import torch
 from torch.utils.data import Dataset, DataLoader , Subset, random_split
 
 class NgramDataset :
-    def __init__(self, window_size) :
+    def __init__(self, v_size, window_size) :
+        self.v_size = v_size
         self.window_size = window_size
 
     def get_data(self, idx_data) :
-        context_data = []
-        for i in range(len(idx_data)) :
+        co_occ = np.zeros((self.v_size, self.v_size))
+        mid_point = int(self.window_size/2)
+
+        for i in range(idx_data) :
             idx_list = idx_data[i]
-            if len(idx_list) < self.window_size :
+            idx_len = len(idx_list)
+            if idx_len < self.window_size :
                 continue
 
             for j in range(len(idx_list)-self.window_size) :
-                context_data.append(idx_list[j:j+self.window_size])
+                sub_list = idx_list[j:j+self.window_size]
+                center = sub_list[mid_point]
+                context = sub_list[:mid_point] + sub_list[mid_point+1:] 
+                co_occ[center][context] += 1
 
-        random.shuffle(context_data)
-        context_data = np.array(context_data)
-        
-        mid_point = int(self.window_size/2)
-        cen_data = context_data[:,mid_point]
-        neighbor_data = np.hstack([context_data[:,:mid_point],
-                                   context_data[:,mid_point+1:]])
+        con_data = []
+        tar_data = []
+        occ_data = []
 
-        return cen_data, neighbor_data
+        for i in range(self.v_size) :
+            for j in range(self.v_size) :
+                if co_occ[i,j] > 0 :
+                    con_data.append(i)
+                    tar_data.append(j)
+                    occ_data.append(co_occ[i,j])
+
+        return con_data, tar_data, occ_data
     
-class EmbeddingDataset(Dataset) :
-    def __init__(self, cen_data, neighbor_data, val_ratio=0.1) :
-        super(EmbeddingDataset , self).__init__()
-        self.c_data = cen_data
-        self.n_data = neighbor_data
+class GloveDataset(Dataset) :
+    def __init__(self, con_data, tar_data, occ_data, val_ratio=0.1) :
+        super(GloveDataset , self).__init__()
+        assert (len(con_data) == len(tar_data)) and (len(con_data) == len(occ_data))
+        self.con_data = con_data
+        self.tar_data = tar_data
+        self.occ_data = occ_data
         self.val_ratio = val_ratio
 
     def __len__(self) :
-        return len(self.c_data)
+        return len(self.con_data)
 
     def __getitem__(self , idx) :
-        return self.c_data[idx], self.n_data[idx]
-    
+        con_idx = self.con_data[idx]
+        tar_idx = self.tar_data[idx]
+        occ_idx = self.occ_data[idx]
+
+        return {'con' : con_idx, 'tar' : tar_idx, 'occ' : occ_idx}
+
     def split(self) :
         n_val = int(len(self) * self.val_ratio)
         n_train = len(self) - n_val
