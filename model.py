c39856aa617d886419271a5ce8930d9c34966182
sangha110495 <48673702+sangHa0411@users.noreply.github.com>
2021-09-15 04:39:56 +0900

    Update model.py

--- model.py
+++ model.py
@@ -2,10 +2,6 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-import torch
-import torch.nn as nn
-import torch.nn.functional as F
-
 class Glove(nn.Module) :
     def __init__(self, em_size, v_size) :
         super(Glove, self).__init__()
@@ -26,16 +22,17 @@ class Glove(nn.Module) :
        
     # con_tensor, tar_tensor : (batch_size, 1)
     def forward(self, con_tensor, tar_tensor) :
-        # (batch_size, 1 , embedding_dim)
-        con_em = self.con_em(con_tensor)
-        tar_em = self.tar_em(tar_tensor)
-        # (batch_size, 1, 1)
-        con_b = self.con_b(con_tensor)
-        tar_b = self.tar_b(tar_tensor)
+        con_tensor = con_tensor.unsqueeze(1)
+        tar_tensor = tar_tensor.unsqueeze(1)
+        
+        con_em = self.con_em(con_tensor) # context embedding
+        tar_em = self.tar_em(tar_tensor) # target embedding
+        con_b = self.con_b(con_tensor) # context bias
+        tar_b = self.tar_b(tar_tensor) # target bias
 
         tar_em_T = torch.transpose(tar_em, 1, 2) # (batch_size, embedding_dim, 1)
         w = torch.matmul(con_em, tar_em_T) # (batch_size, 1, 1)
 
         o_tensor = w + con_b + tar_b # (batch_size, 1, 1)
-        o_tensor = torch.transpose(o_tensor, (-1,))
+        o_tensor = torch.reshape(o_tensor, (-1,))
         return o_tensor
