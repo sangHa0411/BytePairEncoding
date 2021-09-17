0ece47adef0f6017b9c434835552305e7e35071c
sangha <sangha110495@gmail.com>
2021-09-18 02:16:12 +0900

    Update model.py

--- model.py
+++ model.py
@@ -18,6 +18,7 @@ class SkipGram(nn.Module) :
         self.init_param()
         
     def init_param(self) :
+        nn.init.normal_(self.em_layer.weight, mean=0.0, std=0.1)
         for m in self.modules() :
             if isinstance(m, nn.Linear) :
                 nn.init.xavier_normal_(m.weight)
@@ -27,5 +28,18 @@ class SkipGram(nn.Module) :
         em_tensor = self.em_layer(in_tensor)
         o_tensor = self.o_layer(em_tensor)
         o_tensor = torch.reshape(o_tensor, (-1,self.w_size-1,self.v_size))
-
         return o_tensor
+
+    def get_weight(self) :
+        em_weight = self.em_layer.weight
+        o_weight = self.o_layer.weight
+        o_weight = torch.reshape(o_weight, (self.v_size, self.w_size-1, self.em_size))
+        o_weight = torch.mean(o_weight, dim=1)
+        return (em_weight + o_weight) / 2
+
+
+    def get_bias(self) :
+        o_bias = self.o_layer.bias
+        o_bias = torch.reshape(o_bias, (self.v_size, self.w_size-1))
+        o_bias = torch.mean(o_bias, dim=1)
+        return o_bias
