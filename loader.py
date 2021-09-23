6a62114771f03da92861b275242a483c51e4e758
sangha110495 <48673702+sangHa0411@users.noreply.github.com>
2021-09-23 23:45:10 +0900

    Update loader.py

--- loader.py
+++ loader.py
@@ -1,3 +1,4 @@
+import os
 import json
 
 def read_data(file_path) :
@@ -12,4 +13,22 @@ def get_data(json_data) :
         data_list = doc['utterance']
         dialogue = [data['form'] for data in data_list]
         text_data.extend(dialogue)
+    return text_data
+
+def load_data(dir_path) :
+    data_list = os.listdir(dir_path)
+    json_data = []
+    for data in data_list :
+        if data.endswith('.json') :
+            file_path = os.path.join(dir_path, data)
+            try :
+                json_file = read_data(file_path)
+                json_data.append(json_file)
+            except UnicodeDecodeError :
+                continue
+                
+    text_data = []
+    for json_file in json_data :
+        text_list = get_data(json_file)
+        text_data.extend(text_list)
     return text_data
